7af104a1b6fe2edeb0823a58932ca777772ed9cc
John DH. Park <john.dh.park@me.com>
2022-07-06 09:30:51 -0700

    Added eslint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
-
-import './App.css';
+import { useCallback, useState } from "react";
+import "./App.css";
+import DemoOut from "./components/Demo/DemoOut";
+import Button from "./components/UI/Button/Button";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
+
+  const memoizedToggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const setAllowToggleHandler = () => {
+    setAllowToggle(true);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      <DemoOut show={showParagraph} />
+      <Button onClick={memoizedToggleParagraphHandler}>
+        Toggle Paragraph!
+      </Button>
+      <Button onClick={setAllowToggleHandler}>Allow Toggling</Button>
     </div>
   );
 }
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
+import classes from "./Button.module.css";
 
-import classes from './Button.module.css';
+const Button = ({ className, type, onClick, disabled, children }) => (
+  <button
+    type={type}
+    className={`${classes.button} ${className}`}
+    onClick={onClick}
+    disabled={disabled}
+  >
+    {children}
+  </button>
+);
 
-const Button = (props) => {
-  return (
-    <button
-      type={props.type || 'button'}
-      className={`${classes.button} ${props.className}`}
-      onClick={props.onClick}
-      disabled={props.disabled}
-    >
-      {props.children}
-    </button>
-  );
+Button.propTypes = {
+  type: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  children: PropTypes.node,
 };
 
-export default Button;
+Button.defaultProps = {
+  type: "button",
+  disabled: false,
+  className: "",
+  children: null,
+};
+
+export default React.memo(Button);
